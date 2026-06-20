c20332e0e86b99c650ea68dcd7f3dfb0c6ca0ecf
GOAT <lucasonyiego283@gmail.com>
2026-06-20 20:03:59 +0300

    codecrafters submit [skip ci]

--- app/commands.go
+++ app/commands.go
@@ -78,7 +78,7 @@ func execute(userInput string) bool{
 }
 
 func handleEcho(args []string){
-	    fmt.Println(len(args))
+	    
 	    if len(args)>0{
 			 fmt.Println(strings.Join(args," "))
 		 }else{
