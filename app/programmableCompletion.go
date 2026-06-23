251efe520192f0c1c53d8b4632f45ff0fc9c6a28
GOAT <lucasonyiego283@gmail.com>
2026-06-23 15:31:38 +0300

    programmable completion: registering completions

--- app/programmableCompletion.go
+++ app/programmableCompletion.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 
+var completions =make(map[string] string)
+
+
 func complete(args []string){
 
 	
 	   if len(args)<1{
-			  fmt.Println("Complete expected an argument ")
+			  fmt.Println("Complete expected a flag i.e -p ")
 			  return
 		}
 		
@@ -18,7 +21,34 @@ func complete(args []string){
 					  return
 				}
 
-				fmt.Printf("complete: %s: no completion specification\n",args[1])
+				printCompletion(args[1])
+
+
 		}
 
+		if args[0]=="-C"{
+			   if len(args)<3{
+					 fmt.Println("flag -C expects two arguments:path and specification name")
+					 return
+				}
+
+				registerCompletion(args[1],args[2])
+		}
+
+}
+
+
+func printCompletion(completionName string){
+      path,exists:=completions[completionName]
+
+		if exists{
+			  fmt.Printf("complete -C '%s' %s\n",path,completionName)
+		}else{
+           fmt.Printf("complete: %s: no completion specification\n",completionName)
+		}
+}
+
+
+func registerCompletion(path string,completionName string){
+	    completions[completionName]=path
 }
